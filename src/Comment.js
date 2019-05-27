4e05c7a5a563c86a129e40b262ee9cf28d7afea7
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-27 17:57:15 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,14 @@
 // add Comment component here
+import React, { Component } from 'react';
+
+
+export default class Comment extends Component {
+    render() {
+        <div>
+
+
+        </div>
+
+
+    }
+}
